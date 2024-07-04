a0eb92804197aeb8ce2229e8f97b2a63a39012f2
Teguh Santoso <teguhsantoso@sman3kediri.sch.id>
2024-07-04 21:54:10 +0700

    Update provinsi.ts

--- pages/api/provinsi.ts
+++ pages/api/provinsi.ts
@@ -1,6 +1,8 @@
 import { getProvince } from '../../helper/Province' 
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const runtime = 'edge';
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
