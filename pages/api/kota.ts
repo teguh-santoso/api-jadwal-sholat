1d7932152f686c28b1076db490d69bfa5b4ae00c
Teguh Santoso <teguhsantoso@sman3kediri.sch.id>
2024-07-04 21:53:51 +0700

    Update kota.ts

--- pages/api/kota.ts
+++ pages/api/kota.ts
@@ -2,6 +2,8 @@ import { searchProvince } from '../../helper/Province'
 import { searchCity} from '../../helper/Network'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+export const runtime = 'edge';
+
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
